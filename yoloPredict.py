ab5e565b343e21448f7afb38da9074d0d80dffc3
ElectriPixie <ElectriPixie@gmail.com>
2025-02-07 15:50:55 -0500

    added argparse arguments

--- yoloPredict.py
+++ yoloPredict.py
@@ -1,14 +1,23 @@
 import torch
 from ultralytics import YOLO
+import argparse
+
+# Create argparse
+parser = argparse.ArgumentParser(description="YOLO model prediction")
+parser.add_argument("--trained_model", type=str, default="trained_model.pt", help="Path to the trained model")
+parser.add_argument("--image_path", type=str, default="image.jpg", help="Path to the image to make predictions on")
+
+# Parse arguments
+args = parser.parse_args()
 
 # Load the trained model
-trained_model = YOLO("trained_model.pt")
+trained_model = YOLO(args.trained_model)
 
 # Set the device (CPU or GPU)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Make predictions
-predictions = trained_model("image.jpg")
+predictions = trained_model(args.image_path)
 
 # Print predictions
 print(predictions)
--- yoloTrain.py
+++ yoloTrain.py
@@ -1,26 +1,36 @@
 import os
 import torch
 from ultralytics import YOLO
+import argparse
+
+# Create argparse
+parser = argparse.ArgumentParser(description="YOLO model training")
+parser.add_argument("--trained_model", type=str, default="trained_model.pt", help="Path to save the trained model")
+parser.add_argument("--epochs", type=int, default=10, help="Number of epochs to train the model")
+parser.add_argument("--lr0", type=float, default=0.01, help="Initial learning rate")
+parser.add_argument("--batch", type=int, default=32, help="Batch size for training")
+parser.add_argument("--imgsz", type=int, default=640, help="Image size for training")
+parser.add_argument("--dataset", type=str, default="coco8.yaml", help="Path to the training dataset")
+
+# Parse arguments
+args = parser.parse_args()
 
 # Set the device (CPU or GPU)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load the YOLO model
-model = YOLO("yolo11n.pt")
-
-# Set the training dataset
-dataset = "coco8.yaml"
+model = YOLO(args.trained_model)
 
 # Train the model
 model.train(
-    data=dataset,
-    epochs=10,
-    model="yolo11n.pt",
-    lr0=0.01,
-    batch=32,
-    imgsz=640,
+    data=args.dataset,
+    epochs=args.epochs,
+    model=args.trained_model,
+    lr0=args.lr0,
+    batch=args.batch,
+    imgsz=args.imgsz,
 )
 model.val()
 
 # Save the trained model
-model.save("trained_model.pt")
+model.save(args.trained_model)
