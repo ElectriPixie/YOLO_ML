621e56b7854d2c228ce039b5150c7dec1f362082
ElectriPixie <ElectriPixie@gmail.com>
2025-02-07 13:21:37 -0500

    added validation

--- yoloTrain.py
+++ yoloTrain.py
@@ -12,20 +12,17 @@ model = YOLO("yolo11n.pt")
 #dataset = "coco8.yaml"
 # Set the training and validation datasets
 train_dataset = "coco8-train.yaml"
-val_dataset = "coco8-val.yaml"
 
 # Train the model
 model.train(
     data=train_dataset,
-    val_data=val_dataset,
     epochs=10,
     model="yolo11n.pt",
     lr0=0.01,
     batch=32,
     imgsz=640,
-    val_batch=32
-    val_imgsz=640
 )
+model.val()
 
 # Save the trained model
 model.save("trained_model.pt")
